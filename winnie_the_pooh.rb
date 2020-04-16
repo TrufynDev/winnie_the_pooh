4dd9cb2567076885e6a6f941314707d2ce96f27c
TrufynDev <Trufyn.Stanislav@gmail.com>
2020-04-16 21:27:36 +0200

    instal version

---
+++ winnie_the_pooh.rb
@@ -0,0 +1,14 @@
+def count_honeypots
+  honeypots = []
+  3010000.times do |i|
+    honeypots << "Honey pot #{i}"
+  end
+end
+
+puts "Winnie the Pooh is trying to sleep..."
+
+count_honeypots()
+
+GC.start
+
+gets
